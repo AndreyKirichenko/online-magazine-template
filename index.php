c974fa139223a093ad8031fecaf199d3423daae2
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2017-02-26 17:20:07 +0300

    #9 added fixes for css and js including, fixed teaser link

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+    <div class="page">
+        <section class="page__section">
+            <main class="page__main">
+                <div class="teasers">
+                    <h3 class="teasers__header">Читайте в номере</h3>
+                    <ul class="teasers__list">
+                        <?php while ( have_posts() ) : the_post(); ?>
+                            <li class="teasers__item">
+                            <a href="<?php echo get_permalink(); ?>" class="teasers__item-link">
+                                <?php
+                                    $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'your_thumb_handle' );
+                                ?>
+                                <img src="<?php echo $thumbnail['0']; ?>" class="teasers__item-img">
+                                <div class="teasers__item-info">
+                                    <div class="info">
+                                        <time datetime="<?php the_time('d-m-Y'); ?>" pubtime="pubtime" class="info__pubtime"><?php the_time('d-m-Y'); ?></time>
+                                        <div class="info__category"></div>
+                                    </div>
+                                </div>
+
+                                <h3 class="teasers__item-header">
+                                    <?php the_title(); ?>
+                                </h3>
+
+                                <div class="teasers__item-text">
+                                    <?php the_excerpt(); ?>
+                                </div>
+
+                                <div class="teasers__item-button">Читать</div>
+                            </a>
+                        </li>
+                        <?php endwhile; ?>
+                    </ul>
+
+                    <?php oriolo_pagination(); ?>
+                </div>
+            </main>
+            <aside class="page__side">
+                <?php get_sidebar( $name ); ?>
+            </aside>
+        </section>
+    </div>
+<?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -1,73 +1,60 @@
 <div class="popular">
     <h3 class="popular__header">Популярное</h3>
+
     <ul class="popular__list">
         <li class="popular__item">
-            <a href="" class="popular__link">
-                <div style="background-image: url('http://kulick.ru/wp-content/uploads/2017/01/249-620x274.jpg')" class="popular__img-wrapper"></div>
-                <div class="popular__text"> Учимся искажать историю вместе с создателями «Вики... </div>
+            <a href="/theory/freethenipple" class="popular__link">
+                <div style="background-image: url('/wp-content/uploads/2017/01/249-620x274.jpg')" class="popular__img-wrapper"></div>
+                <div class="popular__text">Обнаженные женские соски. Где это уместно, а где нет?</div>
             </a>
         </li>
 
         <li class="popular__item">
-            <a href="" class="popular__link">
-                <div style="background-image: url('http://kulick.ru/wp-content/uploads/2017/01/249-620x274.jpg')" class="popular__img-wrapper"></div>
-                <div class="popular__text"> Учимся искажать историю вместе с создателями «Вики... </div>
+            <a href="/theory/zlo-privlekatelno" class="popular__link">
+                <div style="background-image: url('/wp-content/uploads/2017/01/244.jpg'); background-position: 0%;" class="popular__img-wrapper"></div>
+                <div class="popular__text">Неужели зло так привлекательно?</div>
             </a>
         </li>
 
         <li class="popular__item">
-            <a href="" class="popular__link">
-                <div style="background-image: url('http://kulick.ru/wp-content/uploads/2017/01/249-620x274.jpg')" class="popular__img-wrapper"></div>
-                <div class="popular__text"> Учимся искажать историю вместе с создателями «Вики... </div>
+            <a href="/praktika/assistent-prodyusera" class="popular__link">
+                <div style="background-image: url('http://mostmag.ru/wp-content/uploads/2017/01/111.jpg')" class="popular__img-wrapper"></div>
+                <div class="popular__text">Ассистент продюсера: пришел, увидел, победил!</div>
             </a>
         </li>
 
         <li class="popular__item">
-            <a href="" class="popular__link">
-                <div style="background-image: url('http://kulick.ru/wp-content/uploads/2017/01/249-620x274.jpg')" class="popular__img-wrapper"></div>
-                <div class="popular__text"> Учимся искажать историю вместе с создателями «Вики... </div>
+            <a href="/theory/fenomen-seriala" class="popular__link">
+                <div style="background-image: url('http://mostmag.ru/wp-content/uploads/2016/09/1425241690.jpg')" class="popular__img-wrapper"></div>
+                <div class="popular__text">Феномен сериала: почему сериалы побеждают большое кино?</div>
             </a>
         </li>
 
         <li class="popular__item">
-            <a href="" class="popular__link">
-                <div style="background-image: url('http://kulick.ru/wp-content/uploads/2017/01/249-620x274.jpg')" class="popular__img-wrapper"></div>
-                <div class="popular__text"> Учимся искажать историю вместе с создателями «Вики... </div>
+            <a href="/theory/fargo" class="popular__link">
+                <div style="background-image: url('http://mostmag.ru/wp-content/uploads/2016/12/fargo.jpg')" class="popular__img-wrapper"></div>
+                <div class="popular__text">Исчезновение положительного героя в сериалах: «Фарго»</div>
             </a>
         </li>
 
         <li class="popular__item">
-            <a href="" class="popular__link">
-                <div style="background-image: url('http://kulick.ru/wp-content/uploads/2017/01/249-620x274.jpg')" class="popular__img-wrapper"></div>
-                <div class="popular__text"> Учимся искажать историю вместе с создателями «Вики... </div>
+            <a href="/praktika/top-5-knig-dlya-pisateley" class="popular__link">
+                <div style="background-image: url('http://mostmag.ru/wp-content/uploads/2016/07/y46eshufngklpryavoa.jpg')" class="popular__img-wrapper"></div>
+                <div class="popular__text">Как научиться писать? Топ-5 книг для начинающих писателей.</div>
             </a>
         </li>
 
         <li class="popular__item">
-            <a href="" class="popular__link">
-                <div style="background-image: url('http://kulick.ru/wp-content/uploads/2017/01/249-620x274.jpg')" class="popular__img-wrapper"></div>
-                <div class="popular__text"> Учимся искажать историю вместе с создателями «Вики... </div>
+            <a href="/theory/kartochnyiy-domik" class="popular__link">
+                <div style="background-image: url('http://mostmag.ru/wp-content/uploads/2016/10/shablon_dlya_sayta_tergrgxt-6.jpg"')" class="popular__img-wrapper"></div>
+                <div class="popular__text">Исчезновение положительного героя в сериалах: «Карточный домик»</div>
             </a>
         </li>
 
         <li class="popular__item">
-            <a href="" class="popular__link">
-                <div style="background-image: url('http://kulick.ru/wp-content/uploads/2017/01/249-620x274.jpg')" class="popular__img-wrapper"></div>
-                <div class="popular__text"> Учимся искажать историю вместе с создателями «Вики... </div>
-            </a>
-        </li>
-
-        <li class="popular__item">
-            <a href="" class="popular__link">
-                <div style="background-image: url('http://kulick.ru/wp-content/uploads/2017/01/249-620x274.jpg')" class="popular__img-wrapper"></div>
-                <div class="popular__text"> Учимся искажать историю вместе с создателями «Вики... </div>
-            </a>
-        </li>
-
-        <li class="popular__item">
-            <a href="" class="popular__link">
-                <div style="background-image: url('http://kulick.ru/wp-content/uploads/2017/01/249-620x274.jpg')" class="popular__img-wrapper"></div>
-                <div class="popular__text"> Учимся искажать историю вместе с создателями «Вики... </div>
+            <a href="/theory/vaginalnoe-iskusstvo" class="popular__link">
+                <div style="background-image: url('http://mostmag.ru/wp-content/uploads/2016/10/101.jpg')" class="popular__img-wrapper"></div>
+                <div class="popular__text">Вагинальное искусство</div>
             </a>
         </li>
     </ul>
